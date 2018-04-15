f33eb8a18ac7d7b940d5fb87caab3af12ca2229f
ugur <ucekmez@gmail.com>
2018-04-15 13:40:10 +0300

    fixes

--- PoCs/colorization/run.sh
+++ PoCs/colorization/run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-printf '\n\nImages in this dir :'
+#printf '\n\nImages in this dir :'
 
-{
-  printf '\n%s' $(ls /data/*.png | sed -e 's/\/.*\///g')
-} || {
-  printf '\n%s' $(ls /data/*.jpg | sed -e 's/\/.*\///g')
-} || {
-  printf '\n%s' $(ls /data/*.jpeg | sed -e 's/\/.*\///g')
-}
+# {
+#  printf '\n%s' $(ls /data/*.png | sed -e 's/\/.*\///g')
+#} || {
+#  printf '\n%s' $(ls /data/*.jpg | sed -e 's/\/.*\///g')
+#} || {
+#  printf '\n%s' $(ls /data/*.jpeg | sed -e 's/\/.*\///g')
+#}
+#
+#printf "\n\nEnter image name to colorize it: "
+#read image
 
-printf "\n\nEnter image name to colorize it: "
-read image
+image=$1
 
 outfilename="${image%.*}"
 outextension="${image##*.}"
 outfile="${outfilename}_out.${outextension}"
 
 /root/torch/install/bin/th colorize.lua /data/$image /data/$outfile
-
-printf "\nColorized image have been saved as %s\n\n" $outfile
